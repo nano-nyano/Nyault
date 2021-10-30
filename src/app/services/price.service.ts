ca097cda27b234ff71c880e65f46a6b94c68b594
JeanOUINA <jeanouina@lightcord.org>
2021-10-30 23:29:12 +0200

    linting

--- src/app/services/price.service.ts
+++ src/app/services/price.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {BehaviorSubject} from 'rxjs';
-import BigNumber from "bignumber.js"
+import BigNumber from 'bignumber.js';
 
 @Injectable()
 export class PriceService {
