c3ee4d77061f024d2caf794da09ba4d7fab1ca4a
qwahzi <qwahzi@gmail.com>
2021-11-04 22:54:55 -0500

    Update nyanoToLocale.pipe.ts

--- src/app/pipes/nyanoToLocale.pipe.ts
+++ src/app/pipes/nyanoToLocale.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class nyanoToLocalePipe implements PipeTransform {
   transform(value: any, args?: any): any {
-    console.log('value: ', value, '; value.toLocaleString(): ', value.toLocaleString());
+    console.log('typeof: ', typeof value, 'value: ', value, '; value.toLocaleString(): ', value.toLocaleString(), 'typeof: ', typeof value.toLocaleString());
     return value.toLocaleString();
   }
 }
