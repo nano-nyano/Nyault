bd681a7cc33463436175f756d19e3db4ef5231f3
qwahzi <qwahzi@gmail.com>
2021-11-06 13:31:43 -0500

    Fixing nanoToLocale

--- src/app/pipes/nyanoToLocale.pipe.ts
+++ src/app/pipes/nyanoToLocale.pipe.ts
@@ -1,10 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'nyanoToLocalePipe'
+  name: 'nyanoToLocale'
 })
 
-export class nyanoToLocalePipe implements PipeTransform {
+export class nyanoToLocale implements PipeTransform {
   transform(value: any, args?: any): any {
     return Number(value).toLocaleString("en-US", {maximumFractionDigits: 0});
   }
