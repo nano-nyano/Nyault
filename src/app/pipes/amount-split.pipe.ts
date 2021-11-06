854c86c5777bfe283df3a0dbace01ced48126845
JeanOUINA <jeanouina@lightcord.org>
2021-11-06 11:00:57 +0100

    fix linting ig

--- src/app/pipes/amount-split.pipe.ts
+++ src/app/pipes/amount-split.pipe.ts
@@ -13,14 +13,14 @@ function getClientLocale() {
 const currentLocale = getClientLocale();
 
 function getDecimalSeparator(currentLocale_arg) {
-    const numberWithDecimalSeparator = 1.1;
-    return Intl.NumberFormat(currentLocale_arg)
-        .formatToParts(numberWithDecimalSeparator)
-        .find(part => part.type === 'decimal')
-        .value;
+  const numberWithDecimalSeparator = 1.1;
+  return Intl.NumberFormat(currentLocale_arg)
+    .formatToParts(numberWithDecimalSeparator)
+    .find(part => part.type === 'decimal')
+    .value;
 }
 
-let separator = getDecimalSeparator(currentLocale);
+const separator = getDecimalSeparator(currentLocale);
 
 @Pipe({
   name: 'amountsplit'
